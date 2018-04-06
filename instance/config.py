4829351c6c3cd6b08b699aa5b3b6eb7653ed01e6
Federico Glaudo <dario2994@gmail.com>
2018-04-07 00:19:55 +0200

    Adding CONFIG_FOLDER in config.py

--- instance/config.py
+++ instance/config.py
@@ -1,16 +1,18 @@
+CONFIG_FOLDER = ''
+
 DEBUG = True
-TEAMS_FILE_PATH = 'test_data/teams.txt'
-TABLE_FILE_PATHS = {'T1': 'test_data/T1.json',
-                    'T2': 'test_data/T2.json',
-                    'T3': 'test_data/T3.json',
-                    'T4': 'test_data/T4.json',
-                    'T5': 'test_data/T5.json',
-                    'T6': 'test_data/T6.json',
-                    'T7': 'test_data/T7.json',
-                    'T8': 'test_data/T8.json',
-                    'T9': 'test_data/T9.json',
-                    'T10': 'test_data/T10.json',
-                    'T11': 'test_data/T11.json',
-                    'T12': 'test_data/T12.json'}
-HISTORY_FILE_PATH = 'test_data/history.csv'
-AUTHENTICATION_FILE_PATH = 'test_data/auth_list.json'
+TEAMS_FILE_PATH = CONFIG_FOLDER + 'test_data/teams.txt'
+TABLE_FILE_PATHS = {'T1': CONFIG_FOLDER + 'test_data/T1.json',
+                    'T2': CONFIG_FOLDER + 'test_data/T2.json',
+                    'T3': CONFIG_FOLDER + 'test_data/T3.json',
+                    'T4': CONFIG_FOLDER + 'test_data/T4.json',
+                    'T5': CONFIG_FOLDER + 'test_data/T5.json',
+                    'T6': CONFIG_FOLDER + 'test_data/T6.json',
+                    'T7': CONFIG_FOLDER + 'test_data/T7.json',
+                    'T8': CONFIG_FOLDER + 'test_data/T8.json',
+                    'T9': CONFIG_FOLDER + 'test_data/T9.json',
+                    'T10': CONFIG_FOLDER + 'test_data/T10.json',
+                    'T11': CONFIG_FOLDER + 'test_data/T11.json',
+                    'T12': CONFIG_FOLDER + 'test_data/T12.json'}
+HISTORY_FILE_PATH = CONFIG_FOLDER + 'test_data/history.csv'
+AUTHENTICATION_FILE_PATH = CONFIG_FOLDER + 'test_data/auth_list.json'
